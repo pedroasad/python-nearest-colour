6f30eaacb75ebd51f3c2455a36f49793a4a9a00d
Pedro Asad <psa.exe@gmail.com>
2022-01-13 19:06:01 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Nearest Colours
+
+[![](https://img.shields.io/badge/version-0.1.0-orange)](https://pypi.org/project/nearest-colour)
+![](https://img.shields.io/badge/Python-&ge;3.8-blue)
+
+Find the nearest W3C/X11 named colors to a given color.
+This module, `nearest_colour`, provides two functions:
+
+```python
+from typing import List, Literal, Union
+from colour import Color
+
+ColorSpace = Literal["hsv", "rgb", "yiq", "hls"]
+
+def nearest_x11(color: Union[Color, str], n: int = 1, space: ColorSpace = "hls") -> List[Color]:
+    pass
+
+
+def nearest_w3c(color: Union[Color, str], n: int = 1, space: ColorSpace = "hls") -> List[Color]:
+    pass
+```
+
+Each will return the `n` colors that are closest (Euclidean distance) to `color` in the specified color-`space` from either the set of W3C web colors or the set of Unix X11 colors.
+Web colors are [standardized by W3C][W3C colors], whereas Unix X11 colors are [defined in the X11 source-code][X11 colors].
+**Note:** these two sets of colors are almost entirely overlapping, but not completely.
+Colors may be provided as either [colour] `Color` objects, or as W3C colors strings.
+The default color-space for distance computation is HSL, which is perceptually uniform and therefore returns colors which are perceptually closer to the given color.
+A list is always returned, even if `n == 1`, and the ordering is from most similar to least similar.
+If you pass a large wnough integer (say, 256), you'll get the ranking for all colors in the respective set.
+
+[colour]: https://pypi.org/project/colour/
+[X11 colors]: https://gitlab.freedesktop.org/xorg/xserver/blob/master/os/oscolor.c
+[W3C colors]: https://www.w3.org/wiki/CSS/Properties/color/keywords
---
+++ nearest_colours/__init__.py
@@ -0,0 +1,71 @@
+import colorsys
+from pathlib import Path
+from typing import List, Literal, Tuple, Union
+
+import numpy as np
+import pandas as pd
+from colour import Color
+
+__version__ = "0.1.0"
+ColorSpace = Literal["hsv", "rgb", "yiq", "hls"]
+Profile = Literal["x11", "w3c"]
+
+
+def nearest_x11(color: Union[Color, str], n: int = 1, space: ColorSpace = "hls") -> List[Color]:
+    return _nearest(Color(color), n, space, color_table=_x11_colors)
+
+
+def nearest_w3c(color: Union[Color, str], n: int = 1, space: ColorSpace = "hls") -> List[Color]:
+    return _nearest(Color(color), n, space, color_table=_w3c_colors)
+
+
+def _nearest(color: Color, n: int = 1, space: ColorSpace = "hls", *, color_table: pd.DataFrame) -> List[Color]:
+    if space == "rgb":
+        color_point = color.rgb
+    else:
+        color_point = getattr(colorsys, f"rgb_to_{space}")(*color.rgb)
+
+    unique_colors = color_table.groupby("hex").first()
+    points = unique_colors[space].values.astype(float)
+    points_sq_dist = np.sum((points - color_point)**2, axis=1)
+    argsort = np.argsort(points_sq_dist)
+    ranked_colors = unique_colors.iloc[argsort]
+    n_nearest = ranked_colors.iloc[:n]
+    result = n_nearest["color", ""].tolist()
+
+    return result
+
+
+def _load_colors(profile: Profile) -> pd.DataFrame:
+    _package_dir = Path(__file__).parent
+    table = pd.read_csv(_package_dir / f"{profile}.csv")
+
+    columns = pd.MultiIndex.from_tuples([
+        ("name", ""),
+        ("hex", ""),
+        ("color", ""),
+        *(
+            (space, letter)
+            for space in ["rgb", "hls", "hsv", "yiq"]
+            for letter in space
+        )
+    ])
+    colors = pd.DataFrame(columns=columns)
+
+    colors["name", ""] = table["name"]
+    colors["hex", ""] = table["hex"]
+    colors["color", ""] = table["hex"].apply(Color)
+
+    for i, row in colors.iterrows():
+        rgb = row["color", ""].rgb
+
+        colors.loc[i, "rgb"] = rgb
+        colors.loc[i, "hsv"] = colorsys.rgb_to_hsv(*rgb)
+        colors.loc[i, "hls"] = colorsys.rgb_to_hls(*rgb)
+        colors.loc[i, "yiq"] = colorsys.rgb_to_yiq(*rgb)
+
+    return colors
+
+
+_w3c_colors = _load_colors("w3c")
+_x11_colors = _load_colors("x11")
